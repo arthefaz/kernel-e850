63e746b0874f3db283b3d878d3d3128dcf397428
hwangjae lee <hj-yo.lee@samsung.com>
2017-08-21 16:44:48 +0900

    [9810] fbdev: dpu: the priority of the update thread has changed
    The priority of decon_update_thread has been increased to real time
    task. Because the priority of the other task in the anroid-O version is
    set higher than decon_update_thread, and the operation of
    decon_update_thread is delayed.
    
    The problem of delaying the operation of decon_update_thread after the
    priority change has been solved.
    
    Change-Id: I77ec52adc03f677dd23f0a8107ce9156a1839f40
    Signed-off-by: hwangjae lee <hj-yo.lee@samsung.com>

--- drivers/video/fbdev/exynos/dpu30/decon_core.c
+++ drivers/video/fbdev/exynos/dpu30/decon_core.c
@@ -2913,6 +2913,8 @@ static void decon_destroy_update_thread(struct decon_device *decon)
 
 static int decon_create_update_thread(struct decon_device *decon, char *name)
 {
+	struct sched_param param;
+
 	INIT_LIST_HEAD(&decon->up.list);
 	INIT_LIST_HEAD(&decon->up.saved_list);
 	decon->up_list_saved = false;
@@ -2924,6 +2926,8 @@ static int decon_create_update_thread(struct decon_device *decon, char *name)
 		decon_err("failed to run update_regs thread\n");
 		return PTR_ERR(decon->up.thread);
 	}
+	param.sched_priority = MAX_RT_PRIO - 30;
+	sched_setscheduler_nocheck(decon->up.thread, SCHED_FIFO, &param);
 	kthread_init_work(&decon->up.work, decon_update_regs_handler);
 
 	return 0;
