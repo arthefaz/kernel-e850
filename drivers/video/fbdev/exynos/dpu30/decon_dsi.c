bd16a6d9f073a810a76b66eff872ef134bcf2c8d
Jiun Yu <jiun.yu@samsung.com>
2018-05-28 16:37:59 +0900

    fbdev: dpu20: CONFIG_EXYNOS9820_BTS is change to CONFIG_EXYNOS_BTS
    Previous CONFIG_EXYNOS9820_BTS has SoC dependent name. So, it is changed
    to CONFIG_EXYNOS_BTS without SoC dependency.
    
    Change-Id: Iee1777bf180ad914fc5c8bdb3c39168f535329e8
    Signed-off-by: Jiun Yu <jiun.yu@samsung.com>

--- drivers/video/fbdev/exynos/dpu30/decon_dsi.c
+++ drivers/video/fbdev/exynos/dpu30/decon_dsi.c
@@ -880,7 +880,7 @@ int decon_enter_hiber(struct decon_device *decon)
 		decon_dpp_stop(decon, false);
 	}
 
-#if defined(CONFIG_EXYNOS9820_BTS)
+#if defined(CONFIG_EXYNOS_BTS)
 	decon->bts.ops->bts_release_bw(decon);
 #endif
 
