245dc3fb1f34e5b3530efdc7ac7ae47687ffc304
Min Ho Kim <m8891.kim@samsung.com>
2018-06-05 14:53:12 +0900

    fbdev: dpu20: mem/unmap ref cnt debug feature enable
    Change-Id: I9fa245e6b6948bc547c2a106b78ff18da325aa10
    Signed-off-by: Min Ho Kim <m8891.kim@samsung.com>

--- drivers/video/fbdev/exynos/dpu30/decon_core.c
+++ drivers/video/fbdev/exynos/dpu30/decon_core.c
@@ -268,8 +268,10 @@ static void decon_free_unused_buf(struct decon_device *decon,
 
 	decon_info("%s, win[%d]plane[%d]\n", __func__, win, plane);
 
-	if (dma->attachment && dma->dma_addr)
+	if (dma->attachment && dma->dma_addr) {
 		ion_iovmm_unmap(dma->attachment, dma->dma_addr);
+		dpu_memmap_dec(decon, dma->dma_addr);
+	}
 	if (dma->attachment && dma->sg_table)
 		dma_buf_unmap_attachment(dma->attachment,
 				dma->sg_table, DMA_TO_DEVICE);
@@ -296,6 +298,7 @@ static void decon_free_dma_buf(struct decon_device *decon,
 #endif
 	}
 	ion_iovmm_unmap(dma->attachment, dma->dma_addr);
+	dpu_memmap_dec(decon, dma->dma_addr);
 
 	dma_buf_unmap_attachment(dma->attachment, dma->sg_table,
 			DMA_TO_DEVICE);
@@ -1232,6 +1235,7 @@ static unsigned int decon_map_ion_handle(struct decon_device *decon,
 		decon_err("ion_iovmm_map() failed: %pa\n", &dma->dma_addr);
 		goto err_iovmm_map;
 	}
+	dpu_memmap_inc(decon, dma->dma_addr);
 
 #if defined(CONFIG_SUPPORT_LEGACY_ION)
 	dma->ion_handle = ion_handle;
