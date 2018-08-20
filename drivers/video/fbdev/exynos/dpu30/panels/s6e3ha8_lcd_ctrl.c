4abb39759cc5891dba888c117d0260b746ead8df
Min Ho Kim <m8891.kim@samsung.com>
2018-08-20 09:59:42 +0900

    fbdev: dpu20: s6e3ha9 panel driver added
    - additionally s6e3ha8 modified to support ha9 as well..
    
    Change-Id: Ia18f65021217de3f55d3f2296af39416d7ffeaf8
    Signed-off-by: Min Ho Kim <m8891.kim@samsung.com>

--- drivers/video/fbdev/exynos/dpu30/panels/s6e3ha8_lcd_ctrl.c
+++ drivers/video/fbdev/exynos/dpu30/panels/s6e3ha8_lcd_ctrl.c
@@ -84,59 +84,111 @@ void lcd_init(int id, struct decon_lcd *lcd)
 {
 	dsim_dbg("%s +\n", __func__);
 
-	msleep(5);
+	if (dsim_drvdata[0]->board_info == 1) {
+		msleep(5);
 
-	if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_TEST_KEY_ON_F0,
-				ARRAY_SIZE(SEQ_TEST_KEY_ON_F0)) < 0)
-		dsim_err("fail to write SEQ_TEST_KEY_ON_F0 command.\n");
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_TEST_KEY_ON_F0_HA9,
+					ARRAY_SIZE(SEQ_TEST_KEY_ON_F0_HA9)) < 0)
+			dsim_err("fail to write SEQ_TEST_KEY_ON_F0 command.\n");
 
-	if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_TEST_KEY_ON_FC,
-				ARRAY_SIZE(SEQ_TEST_KEY_ON_FC)) < 0)
-		dsim_err("fail to write SEQ_TEST_KEY_ON_FC command.\n");
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_TEST_KEY_ON_FC_HA9,
+					ARRAY_SIZE(SEQ_TEST_KEY_ON_FC_HA9)) < 0)
+			dsim_err("fail to write SEQ_TEST_KEY_ON_FC command.\n");
 
-	if (dsim_wr_data(id, MIPI_DSI_DSC_PRA, (unsigned long)SEQ_DSC_EN[0], 0) < 0)
-		dsim_err("fail to write SEQ_DSC_EN command.\n");
+		if (dsim_wr_data(id, MIPI_DSI_DSC_PRA, (unsigned long)SEQ_DSC_EN_HA9[0], 0) < 0)
+			dsim_err("fail to write SEQ_DSC_EN command.\n");
 
-	switch (lcd->dsc_slice_num) {
-	case 2:
-		if (dsim_wr_data(id, MIPI_DSI_DSC_PPS, (unsigned long)SEQ_PPS_SLICE2,
-					ARRAY_SIZE(SEQ_PPS_SLICE2)) < 0)
-			dsim_err("fail to write SEQ_PPS_SLICE2 command.\n");
-		break;
-	default:
-		dsim_err("fail to set MIPI_DSI_DSC_PPS command(no slice).\n");
-		break;
+		switch (lcd->dsc_slice_num) {
+			case 2:
+				if (dsim_wr_data(id, MIPI_DSI_DSC_PPS, (unsigned long)SEQ_PPS_SLICE2_HA9,
+							ARRAY_SIZE(SEQ_PPS_SLICE2_HA9)) < 0)
+					dsim_err("fail to write SEQ_PPS_SLICE2 command.\n");
+				break;
+			default:
+				dsim_err("fail to set MIPI_DSI_DSC_PPS command(no slice).\n");
+				break;
+		}
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE,
+					(unsigned long)SEQ_SLEEP_OUT_HA9[0], 0) < 0)
+			dsim_err("fail to send SEQ_SLEEP_OUT command.\n");
+
+		msleep(120);
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_TSP_HSYNC_HA9,
+					ARRAY_SIZE(SEQ_TSP_HSYNC_HA9)) < 0)
+			dsim_err("fail to write SEQ_TSP_HSYNC command.\n");
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE,
+					(unsigned long)SEQ_TE_ON_HA9[0], 0) < 0)
+			dsim_err("fail to send SEQ_TE_ON command.\n");
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+					(unsigned long)SEQ_ERR_FG_HA9[0], (u32)SEQ_ERR_FG_HA9[1]) < 0)
+			dsim_err("fail to send SEQ_ERR_FG command.\n");
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_TE_START_SETTING_HA9,
+					ARRAY_SIZE(SEQ_TE_START_SETTING_HA9)) < 0)
+			dsim_err("fail to write SEQ_TE_START_SETTING command.\n");
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_FFC_HA9,
+					ARRAY_SIZE(SEQ_FFC_HA9)) < 0)
+			dsim_err("fail to write SEQ_FFC command.\n");
+	} else {
+		msleep(5);
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_TEST_KEY_ON_F0,
+					ARRAY_SIZE(SEQ_TEST_KEY_ON_F0)) < 0)
+			dsim_err("fail to write SEQ_TEST_KEY_ON_F0 command.\n");
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_TEST_KEY_ON_FC,
+					ARRAY_SIZE(SEQ_TEST_KEY_ON_FC)) < 0)
+			dsim_err("fail to write SEQ_TEST_KEY_ON_FC command.\n");
+
+		if (dsim_wr_data(id, MIPI_DSI_DSC_PRA, (unsigned long)SEQ_DSC_EN[0], 0) < 0)
+			dsim_err("fail to write SEQ_DSC_EN command.\n");
+
+		switch (lcd->dsc_slice_num) {
+			case 2:
+				if (dsim_wr_data(id, MIPI_DSI_DSC_PPS, (unsigned long)SEQ_PPS_SLICE2,
+							ARRAY_SIZE(SEQ_PPS_SLICE2)) < 0)
+					dsim_err("fail to write SEQ_PPS_SLICE2 command.\n");
+				break;
+			default:
+				dsim_err("fail to set MIPI_DSI_DSC_PPS command(no slice).\n");
+				break;
+		}
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE,
+					(unsigned long)SEQ_SLEEP_OUT[0], 0) < 0)
+			dsim_err("fail to send SEQ_SLEEP_OUT command.\n");
+
+		msleep(120);
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_TSP_HSYNC,
+					ARRAY_SIZE(SEQ_TSP_HSYNC)) < 0)
+			dsim_err("fail to write SEQ_TSP_HSYNC command.\n");
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_SET_AREA,
+					ARRAY_SIZE(SEQ_SET_AREA)) < 0)
+			dsim_err("fail to write SEQ_SET_AREA command.\n");
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE,
+					(unsigned long)SEQ_TE_ON[0], 0) < 0)
+			dsim_err("fail to send SEQ_TE_ON command.\n");
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+					(unsigned long)SEQ_ERR_FG[0], (u32)SEQ_ERR_FG[1]) < 0)
+			dsim_err("fail to send SEQ_ERR_FG command.\n");
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_TE_START_SETTING,
+					ARRAY_SIZE(SEQ_TE_START_SETTING)) < 0)
+			dsim_err("fail to write SEQ_TE_START_SETTING command.\n");
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_FFC,
+					ARRAY_SIZE(SEQ_FFC)) < 0)
+			dsim_err("fail to write SEQ_FFC command.\n");
 	}
-
-	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE,
-				(unsigned long)SEQ_SLEEP_OUT[0], 0) < 0)
-		dsim_err("fail to send SEQ_SLEEP_OUT command.\n");
-
-	msleep(120);
-
-	if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_TSP_HSYNC,
-				ARRAY_SIZE(SEQ_TSP_HSYNC)) < 0)
-		dsim_err("fail to write SEQ_TSP_HSYNC command.\n");
-
-	if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_SET_AREA,
-				ARRAY_SIZE(SEQ_SET_AREA)) < 0)
-		dsim_err("fail to write SEQ_SET_AREA command.\n");
-
-	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE,
-				(unsigned long)SEQ_TE_ON[0], 0) < 0)
-		dsim_err("fail to send SEQ_TE_ON command.\n");
-
-	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
-				(unsigned long)SEQ_ERR_FG[0], (u32)SEQ_ERR_FG[1]) < 0)
-		dsim_err("fail to send SEQ_ERR_FG command.\n");
-
-	if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_TE_START_SETTING,
-				ARRAY_SIZE(SEQ_TE_START_SETTING)) < 0)
-		dsim_err("fail to write SEQ_TE_START_SETTING command.\n");
-
-	if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)SEQ_FFC,
-				ARRAY_SIZE(SEQ_FFC)) < 0)
-		dsim_err("fail to write SEQ_FFC command.\n");
 
 	dsim_dbg("%s -\n", __func__);
 }
@@ -212,62 +264,125 @@ int lcd_dump(int id)
 
 void lcd_mres(int id, int mres_idx, int dsc_en)
 {
-	if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)KEY1_ENABLE,
-			ARRAY_SIZE(KEY1_ENABLE)) < 0) {
-		dsim_err("failed to write KEY1_ENABLE\n");
-		return;
-	}
-	if (dsc_en) {
-		if (dsim_wr_data(id, MIPI_DSI_DSC_PRA,
-					(unsigned long)DSC_EN[1][0], 0) < 0) {
-			dsim_err("failed to write DSC_EN\n");
+	if (dsim_drvdata[0]->board_info == 1) {
+		dsim_info("board info(%d) : s6e3ha9\n", dsim_drvdata[0]->board_info);
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)KEY1_ENABLE_HA9,
+					ARRAY_SIZE(KEY1_ENABLE_HA9)) < 0) {
+			dsim_err("failed to write KEY1_ENABLE\n");
 			return;
 		}
-		if (dsim_wr_data(id, MIPI_DSI_DSC_PPS,
-					(unsigned long)PPS_TABLE[mres_idx],
-					ARRAY_SIZE(PPS_TABLE[mres_idx])) < 0) {
-			dsim_err("failed to write PPS_TABLE[%d]\n", mres_idx);
+		if (dsc_en) {
+			if (dsim_wr_data(id, MIPI_DSI_DSC_PRA,
+						(unsigned long)DSC_EN_HA9[1][0], 0) < 0) {
+				dsim_err("failed to write DSC_EN\n");
+				return;
+			}
+			if (dsim_wr_data(id, MIPI_DSI_DSC_PPS,
+						(unsigned long)PPS_TABLE_HA9[mres_idx],
+						ARRAY_SIZE(PPS_TABLE_HA9[mres_idx])) < 0) {
+				dsim_err("failed to write PPS_TABLE[%d]\n", mres_idx);
+				return;
+			}
+		} else {
+			if (dsim_wr_data(id, MIPI_DSI_DSC_PRA,
+						(unsigned long)DSC_EN_HA9[0][0], 0) < 0) {
+				dsim_err("failed to write DSC_EN\n");
+				return;
+			}
+		}
+
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)KEY1_DISABLE_HA9,
+					ARRAY_SIZE(KEY1_DISABLE_HA9)) < 0) {
+			dsim_err("failed to write KEY1_DISABLE\n");
+			return;
+		}
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE,
+					(unsigned long)CASET_TABLE_HA9[mres_idx],
+					ARRAY_SIZE(CASET_TABLE_HA9[mres_idx])) < 0) {
+			dsim_err("failed to write CASET_TABLE[%d]\n", mres_idx);
+			return;
+		}
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE,
+					(unsigned long)PASET_TABLE_HA9[mres_idx],
+					ARRAY_SIZE(PASET_TABLE_HA9[mres_idx])) < 0) {
+			dsim_err("failed to write PASET_TABLE[%d]\n", mres_idx);
+			return;
+		}
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)KEY2_ENABLE_HA9,
+					ARRAY_SIZE(KEY2_ENABLE_HA9)) < 0) {
+			dsim_err("failed to write KEY2_ENABLE\n");
+			return;
+		}
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE,
+					(unsigned long)SCALER_TABLE_HA9[mres_idx],
+					ARRAY_SIZE(SCALER_TABLE_HA9[mres_idx])) < 0) {
+			dsim_err("failed to write SCALER_TABLE[%d]\n", mres_idx);
+			return;
+		}
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)KEY2_DISABLE_HA9,
+					ARRAY_SIZE(KEY2_DISABLE_HA9)) < 0) {
+			dsim_err("failed to write KEY2_DISABLE\n");
 			return;
 		}
 	} else {
-		if (dsim_wr_data(id, MIPI_DSI_DSC_PRA,
-					(unsigned long)DSC_EN[0][0], 0) < 0) {
-			dsim_err("failed to write DSC_EN\n");
+		dsim_info("board info(%d) : s6e3ha8\n", dsim_drvdata[0]->board_info);
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)KEY1_ENABLE,
+					ARRAY_SIZE(KEY1_ENABLE)) < 0) {
+			dsim_err("failed to write KEY1_ENABLE\n");
 			return;
 		}
-	}
+		if (dsc_en) {
+			if (dsim_wr_data(id, MIPI_DSI_DSC_PRA,
+						(unsigned long)DSC_EN[1][0], 0) < 0) {
+				dsim_err("failed to write DSC_EN\n");
+				return;
+			}
+			if (dsim_wr_data(id, MIPI_DSI_DSC_PPS,
+						(unsigned long)PPS_TABLE[mres_idx],
+						ARRAY_SIZE(PPS_TABLE[mres_idx])) < 0) {
+				dsim_err("failed to write PPS_TABLE[%d]\n", mres_idx);
+				return;
+			}
+		} else {
+			if (dsim_wr_data(id, MIPI_DSI_DSC_PRA,
+						(unsigned long)DSC_EN[0][0], 0) < 0) {
+				dsim_err("failed to write DSC_EN\n");
+				return;
+			}
+		}
 
-	if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)KEY1_DISABLE,
-			ARRAY_SIZE(KEY1_DISABLE)) < 0) {
-		dsim_err("failed to write KEY1_DISABLE\n");
-		return;
-	}
-	if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE,
-				(unsigned long)CASET_TABLE[mres_idx],
-				ARRAY_SIZE(CASET_TABLE[mres_idx])) < 0) {
-		dsim_err("failed to write CASET_TABLE[%d]\n", mres_idx);
-		return;
-	}
-	if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE,
-				(unsigned long)PASET_TABLE[mres_idx],
-				ARRAY_SIZE(PASET_TABLE[mres_idx])) < 0) {
-		dsim_err("failed to write PASET_TABLE[%d]\n", mres_idx);
-		return;
-	}
-	if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)KEY2_ENABLE,
-			ARRAY_SIZE(KEY2_ENABLE)) < 0) {
-		dsim_err("failed to write KEY2_ENABLE\n");
-		return;
-	}
-	if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE,
-				(unsigned long)SCALER_TABLE[mres_idx],
-				ARRAY_SIZE(SCALER_TABLE[mres_idx])) < 0) {
-		dsim_err("failed to write SCALER_TABLE[%d]\n", mres_idx);
-		return;
-	}
-	if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)KEY2_DISABLE,
-			ARRAY_SIZE(KEY2_DISABLE)) < 0) {
-		dsim_err("failed to write KEY2_DISABLE\n");
-		return;
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)KEY1_DISABLE,
+					ARRAY_SIZE(KEY1_DISABLE)) < 0) {
+			dsim_err("failed to write KEY1_DISABLE\n");
+			return;
+		}
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE,
+					(unsigned long)CASET_TABLE[mres_idx],
+					ARRAY_SIZE(CASET_TABLE[mres_idx])) < 0) {
+			dsim_err("failed to write CASET_TABLE[%d]\n", mres_idx);
+			return;
+		}
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE,
+					(unsigned long)PASET_TABLE[mres_idx],
+					ARRAY_SIZE(PASET_TABLE[mres_idx])) < 0) {
+			dsim_err("failed to write PASET_TABLE[%d]\n", mres_idx);
+			return;
+		}
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)KEY2_ENABLE,
+					ARRAY_SIZE(KEY2_ENABLE)) < 0) {
+			dsim_err("failed to write KEY2_ENABLE\n");
+			return;
+		}
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE,
+					(unsigned long)SCALER_TABLE[mres_idx],
+					ARRAY_SIZE(SCALER_TABLE[mres_idx])) < 0) {
+			dsim_err("failed to write SCALER_TABLE[%d]\n", mres_idx);
+			return;
+		}
+		if (dsim_wr_data(id, MIPI_DSI_DCS_LONG_WRITE, (unsigned long)KEY2_DISABLE,
+					ARRAY_SIZE(KEY2_DISABLE)) < 0) {
+			dsim_err("failed to write KEY2_DISABLE\n");
+			return;
+		}
 	}
 }
