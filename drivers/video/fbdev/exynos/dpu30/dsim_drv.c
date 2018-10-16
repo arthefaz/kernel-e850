7bf192c34e6619332a8cd0e6872a72e673bc01a9
Jiun Yu <jiun.yu@samsung.com>
2018-10-16 15:57:05 +0900

    fbdev: dpu30: add 3FA0 panel definitions in dsim driver.
    Change-Id: I6f8e22df4fcf5834ff6eca1d3a3c16f544e5e460
    Signed-off-by: Jiun Yu <jiun.yu@samsung.com>

--- drivers/video/fbdev/exynos/dpu30/dsim_drv.c
+++ drivers/video/fbdev/exynos/dpu30/dsim_drv.c
@@ -1494,6 +1494,8 @@ static void dsim_register_panel(struct dsim_device *dsim)
 	dsim->panel_ops = &s6e3aa2_mipi_lcd_driver;
 #elif IS_ENABLED(CONFIG_EXYNOS_DECON_LCD_EMUL_DISP)
 	dsim->panel_ops = &emul_disp_mipi_lcd_driver;
+#elif IS_ENABLED(CONFIG_EXYNOS_DECON_LCD_S6E3FA0)
+	dsim->panel_ops = &s6e3fa0_mipi_lcd_driver;
 #else
 	dsim->panel_ops = &s6e3ha2k_mipi_lcd_driver;
 #endif
