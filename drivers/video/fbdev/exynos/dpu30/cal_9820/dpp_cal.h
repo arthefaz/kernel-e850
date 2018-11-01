8757db5fe0f4f4e10991405e76b463a4a646d50f
Jiun Yu <jiun.yu@samsung.com>
2018-11-01 10:09:32 +0900

    fbdev: dpu30: SoC specific color format is defined in CAL.
    Change-Id: I0d43cded979836567809141b71f7852aa7f16ea1
    Signed-off-by: Jiun Yu <jiun.yu@samsung.com>

--- drivers/video/fbdev/exynos/dpu30/cal_9820/dpp_cal.h
+++ drivers/video/fbdev/exynos/dpu30/cal_9820/dpp_cal.h
@@ -105,4 +105,6 @@ u32 dpp_reg_get_irq_and_clear(u32 id);
 u32 idma_reg_get_irq_and_clear(u32 id);
 u32 odma_reg_get_irq_and_clear(u32 id);
 
+const struct dpu_fmt *dpu_find_cal_fmt_info(enum decon_pixel_format fmt);
+
 #endif /* __SAMSUNG_DPP_CAL_H__ */
