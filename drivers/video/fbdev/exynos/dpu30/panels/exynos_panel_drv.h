ae04b62281e376ed64bd7b3a52d9bfd4b417dfb0
Jiun Yu <jiun.yu@samsung.com>
2018-12-27 13:18:19 +0900

    fbdev: dpu30: add S6E3FA0 panel driver
    Change-Id: I5acc782226c502c73706855da0347f69b56813f3
    Signed-off-by: Jiun Yu <jiun.yu@samsung.com>

--- drivers/video/fbdev/exynos/dpu30/panels/exynos_panel_drv.h
+++ drivers/video/fbdev/exynos/dpu30/panels/exynos_panel_drv.h
@@ -48,6 +48,7 @@ extern int dpu_panel_log_level;
 extern struct exynos_panel_device *panel_drvdata;
 extern struct exynos_panel_ops panel_s6e3ha9_ops;
 extern struct exynos_panel_ops panel_s6e3ha8_ops;
+extern struct exynos_panel_ops panel_s6e3fa0_ops;
 
 struct exynos_panel_resources {
 	int lcd_reset;
