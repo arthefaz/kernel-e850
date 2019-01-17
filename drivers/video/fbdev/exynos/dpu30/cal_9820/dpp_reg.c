876e263e025ff570c121a6c05af947ab66531c70
Jaehoe Yang <jaehoe.yang@samsung.com>
2019-01-17 13:08:19 +0900

    fbdev: dpu30: fixed compile errors of DPP
    Change-Id: Ief05f9e2374b4c02aff2ad8fe0e72ae745698f42
    Signed-off-by: Jaehoe Yang <jaehoe.yang@samsung.com>

--- drivers/video/fbdev/exynos/dpu30/cal_9820/dpp_reg.c
+++ drivers/video/fbdev/exynos/dpu30/cal_9820/dpp_reg.c
@@ -612,7 +612,7 @@ static int wb_mux_reg_wait_sw_reset_status(u32 id)
 		udelay(10);
 	} while (--cnt);
 
-	dpp_err("[WBMUX] dpp%d timeout sw-reset\n");
+	dpp_err("[WBMUX] dpp%d timeout sw-reset\n", id);
 
 	return -1;
 }
@@ -1025,7 +1025,7 @@ void dma_reg_get_shd_addr(u32 id, u32 shd_addr[], const unsigned long attr)
 		shd_addr[2] = dma_read(id, ODMA_IN_BASE_ADDR_Y2 + DMA_SHD_OFFSET);
 		shd_addr[3] = dma_read(id, ODMA_IN_BASE_ADDR_C2 + DMA_SHD_OFFSET);
 	}
-	dpp_dbg("dpp%d: shadow addr 1p(0x%p) 2p(0x%p) 3p(0x%p) 4p(0x%p)\n",
+	dpp_dbg("dpp%d: shadow addr 1p(0x%x) 2p(0x%x) 3p(0x%x) 4p(0x%x)\n",
 			id, shd_addr[0], shd_addr[1], shd_addr[2], shd_addr[3]);
 }
 
