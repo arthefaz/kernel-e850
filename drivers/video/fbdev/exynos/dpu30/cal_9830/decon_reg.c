20a22cac3d538cd9f17a6089f130fe4e196e94f4
yeongran shin <yr613.shin@samsung.com>
2019-02-27 13:53:33 +0900

    [9830] fbdev: dpu30: removed dpu qactive pll function
    Change-Id: I1215366c61681992c3babacfa77321942e1773c3
    Signed-off-by: yeongran shin <yr613.shin@samsung.com>

--- drivers/video/fbdev/exynos/dpu30/cal_9830/decon_reg.c
+++ drivers/video/fbdev/exynos/dpu30/cal_9830/decon_reg.c
@@ -13,12 +13,6 @@
 #include "../format.h"
 
 /******************* DECON CAL functions *************************/
-static void dpu_reg_set_qactive_pll(u32 id, u32 en)
-{
-	sysreg_write_mask(id, DISP_DPU_TE_QACTIVE_PLL_EN, en ? ~0 : 0,
-			TE_QACTIVE_PLL_EN);
-}
-
 static int decon_reg_reset(u32 id)
 {
 	int tries;
@@ -1439,8 +1433,6 @@ static void decon_reg_init_probe(u32 id, u32 dsi_idx, struct decon_param *p)
 	enum decon_dsi_mode dsi_mode = psr->dsi_mode;
 	u32 overlap_w = 0; /* default=0 : range=[0, 32] & (multiples of 2) */
 
-	dpu_reg_set_qactive_pll(id, true);
-
 	decon_reg_set_clkgate_mode(id, 0);
 
 	decon_reg_set_sram_share(id, DECON_FIFO_04K);
@@ -1784,8 +1776,6 @@ int decon_reg_init(u32 id, u32 dsi_idx, struct decon_param *p)
 			decon_reg_set_trigger(id, psr, DECON_TRIG_DISABLE);
 		return -EBUSY;
 	}
-
-	dpu_reg_set_qactive_pll(id, true);
 
 	decon_reg_set_clkgate_mode(id, 0);
 
