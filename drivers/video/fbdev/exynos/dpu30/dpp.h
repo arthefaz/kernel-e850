7d7951c72a6027b8de101a9d60166a2bc3b2389e
Jaehoe Yang <jaehoe.yang@samsung.com>
2019-04-22 15:53:08 +0900

    fbdev: dpu30: change SBWC attribute bit id from 9 to 10
    SBWC attribute bit is changed from [9] to [10]
    becasue bit[9] is used for WCG.
    
    Change-Id: I00f5d287cfdce37ca83285139f0170f28a36d896
    Signed-off-by: Jaehoe Yang <jaehoe.yang@samsung.com>

--- drivers/video/fbdev/exynos/dpu30/dpp.h
+++ drivers/video/fbdev/exynos/dpu30/dpp.h
@@ -130,7 +130,8 @@ enum dpp_attr {
 	DPP_ATTR_HDR		= 6,
 	DPP_ATTR_C_HDR		= 7,
 	DPP_ATTR_C_HDR10_PLUS	= 8,
-	DPP_ATTR_SBWC		= 9,
+	DPP_ATTR_WCG		= 9,
+	DPP_ATTR_SBWC		= 10,
 
 	DPP_ATTR_IDMA		= 16,
 	DPP_ATTR_ODMA		= 17,
