b5aa678b5862b67ee9c9c742e34cf641ae8e65f2
Dongseong Kim <ds_in_ds.kim@samsung.com>
2019-05-28 20:04:16 +0900

    media: scaler: modify csc_idx for blending
    - blending output is RGB format. If dest format is YCbCr, hardwired csc will occur.
      So, although both source and destination are YCbCr, csc_idx should be CSC_Y2R.
    
    Change-Id: Ia8bc495339488041e3c496a41144aaa3f08d64fb
    Signed-off-by: Dongseong Kim <ds_in_ds.kim@samsung.com>

--- drivers/media/platform/exynos/scaler/scaler-regs.c
+++ drivers/media/platform/exynos/scaler/scaler-regs.c
@@ -625,7 +625,8 @@ void sc_hwset_blend(struct sc_dev *sc, enum sc_blend_op bl_op, bool pre_multi,
 	sc_dbg("src_blend_alpha is 0x%x\n", cfg);
 
 	cfg = readl(sc->regs + SCALER_DST_BLEND_COLOR);
-	get_blend_value(&cfg, sc_bl_op_tbl[idx].dst_color, pre_multi);
+	get_blend_value(&cfg, sc_bl_op_tbl[idx].dst_color,
+			src_blend_cfg->pre_multi);
 	if (g_alpha < 0xff)
 		cfg |= ((INV_SAGA & 0xf) << SCALER_OP_SEL_SHIFT);
 	writel(cfg, sc->regs + SCALER_DST_BLEND_COLOR);
@@ -1101,6 +1102,7 @@ void sc_hwregs_dump(struct sc_dev *sc)
 
 	if (sc->version >= SCALER_VERSION(5, 0, 0))
 		goto end;
+
 
 	/* shadow registers */
 	sc_print_hex_dump(sc, sc->regs + 0x1004, 0x1004 - 0x1004 + 4);
