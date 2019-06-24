18b7e23cfe38bb86e2427b1ed30888f7a7f11b1c
Hyeonseong Gil <hs.gil@samsung.com>
2019-06-24 16:03:12 +0900

    [COMMON] soc: samsung: Sync eyxnos-rgt driver
    Change-Id: Ibfce504ec97ed60f4b13e1e9e4fbd584706b1cb7
    Signed-off-by: Hyeonseong Gil <hs.gil@samsung.com>

--- drivers/soc/samsung/exynos-rgt.c
+++ drivers/soc/samsung/exynos-rgt.c
@@ -42,6 +42,27 @@ struct exynos_rgt_info {
 static struct dentry *exynos_rgt_root;
 static int num_regulators = 0;
 
+#ifdef CONFIG_DEBUG_SNAPSHOT_REGULATOR
+#define REG_MAP_MAX		(0x100)
+
+struct regulator_ss_info {
+	char name[7];
+	unsigned int min_uV;
+	unsigned int uV_step;
+	unsigned int linear_min_sel;
+	unsigned int vsel_reg;
+};
+
+struct exynos_rgt_ss_info {
+	u32 acpm_pmic_cnt;
+	u32 *acpm_reg_cnt;
+	struct regulator_ss_info **regulator_ss;
+	char **reg_map;
+	bool is_reg_map_set;
+};
+struct exynos_rgt_ss_info rgt_ss_info;
+#endif
+
 static const char *rdev_get_name(struct regulator_dev *rdev)
 {
 	if (rdev->desc->name)
@@ -256,15 +277,159 @@ static const struct file_operations exynos_rgt_volt_fops = {
 	.llseek = default_llseek,
 };
 
+#ifdef CONFIG_DEBUG_SNAPSHOT_REGULATOR
+struct regulator_ss_info *get_regulator_ss(u32 rgt_idx, u32 spd_ch)
+{
+	if (spd_ch < rgt_ss_info.acpm_pmic_cnt && rgt_idx < rgt_ss_info.acpm_reg_cnt[spd_ch])
+		return &(rgt_ss_info.regulator_ss[spd_ch][rgt_idx]);
+	else
+		return NULL;
+}
+
+static void set_reg_map(void)
+{
+	u32 idx;
+	int i, j;
+
+	for (i = 0; i < rgt_ss_info.acpm_pmic_cnt; i++) {
+		for (j = 0; j < rgt_ss_info.acpm_reg_cnt[i]; j++) {
+			idx = rgt_ss_info.regulator_ss[i][j].vsel_reg & 0xFF;
+			if (idx == 0)
+				continue;
+
+			if ((rgt_ss_info.reg_map[i][idx]) != 0)
+				pr_err("duplicated set_reg_map [%d] reg_map %x\n", j,
+						rgt_ss_info.reg_map[i][idx]);
+
+			rgt_ss_info.reg_map[i][idx] = j;
+		}
+	}
+
+	rgt_ss_info.is_reg_map_set = true;
+}
+
+static u32 get_reg_id(u32 ch, u32 addr)
+{
+	u32 id;
+
+	if (rgt_ss_info.is_reg_map_set == false)
+		return REG_MAP_MAX;
+
+	if (addr >> 8 != 0x1 || ch >= rgt_ss_info.acpm_pmic_cnt)
+		return REG_MAP_MAX;
+
+	id = rgt_ss_info.reg_map[ch][addr & 0xff];
+	if (id != 0)
+		return id;
+
+	return REG_MAP_MAX;
+}
+
+static u32 get_reg_voltage(struct regulator_ss_info reg_info, u32 selector)
+{
+	if (reg_info.name[0] == 'L')
+		selector = selector & 0x3F;
+
+	return reg_info.min_uV + (reg_info.uV_step * (selector - reg_info.linear_min_sel));
+}
+
+static int prepare_dss_regulator(struct platform_device *pdev)
+{
+	struct device_node *exynos_rgt_np = pdev->dev.of_node;
+	struct device_node *regulators_np, *pmic_np;
+	int ret, ipc_ch, spd_ch, i;
+
+	ret = of_property_read_u32(exynos_rgt_np, "num-acpm-pmic", &(rgt_ss_info.acpm_pmic_cnt));
+	if (ret) {
+		pr_err("%s parse fail on num-acpm-pmic\n", EXYNOS_RGT_PREFIX);
+		return -ENODEV;
+	}
+
+	rgt_ss_info.acpm_reg_cnt = kzalloc(sizeof(u32) * rgt_ss_info.acpm_pmic_cnt, GFP_KERNEL);
+	if (!rgt_ss_info.acpm_reg_cnt) {
+		pr_err("%s mem alloc fail on acpm_reg_cnt.\n", EXYNOS_RGT_PREFIX);
+		return -ENOMEM;
+	}
+
+	regulators_np = of_find_node_by_name(NULL, "regulators");
+	while (regulators_np) {
+		pmic_np = of_get_parent(regulators_np);
+		ret = of_property_read_u32(pmic_np, "acpm-ipc-channel", &ipc_ch);
+		if (!ret) {
+			ret = of_property_read_u32(pmic_np, "acpm-speedy-channel", &spd_ch);
+			if (!ret) {
+				if (spd_ch < rgt_ss_info.acpm_pmic_cnt) {
+					rgt_ss_info.acpm_reg_cnt[spd_ch] = of_get_child_count(regulators_np);
+				}
+			}
+		}
+		regulators_np = of_find_node_by_name(regulators_np, "regulators");
+	}
+
+	rgt_ss_info.regulator_ss = kzalloc(sizeof(struct regulator_ss_info *) * rgt_ss_info.acpm_pmic_cnt, GFP_KERNEL);
+	if (!(rgt_ss_info.regulator_ss)) {
+		pr_err("%s mem alloc fail on *regulator_ss.\n", EXYNOS_RGT_PREFIX);
+		return -ENOMEM;
+	}
+	for (i = 0; i < rgt_ss_info.acpm_pmic_cnt; i++) {
+		rgt_ss_info.regulator_ss[i] = kzalloc(sizeof(struct regulator_ss_info) * rgt_ss_info.acpm_reg_cnt[i], GFP_KERNEL);
+		if (!(rgt_ss_info.regulator_ss[i])) {
+			pr_err("%s mem alloc fail on regulator_ss.\n", EXYNOS_RGT_PREFIX);
+			return -ENOMEM;
+		}
+	}
+
+	rgt_ss_info.reg_map = kzalloc(sizeof(char *) * rgt_ss_info.acpm_pmic_cnt, GFP_KERNEL);
+	if (!(rgt_ss_info.reg_map)) {
+		pr_err("%s mem alloc fail on *reg_map.\n", EXYNOS_RGT_PREFIX);
+		return -ENOMEM;
+	}
+	for (i = 0; i < rgt_ss_info.acpm_pmic_cnt; i++) {
+		rgt_ss_info.reg_map[i] = kzalloc(sizeof(char) * REG_MAP_MAX, GFP_KERNEL);
+		if (!(rgt_ss_info.reg_map[i])) {
+			pr_err("%s mem alloc fail on reg_map.\n", EXYNOS_RGT_PREFIX);
+			return -ENOMEM;
+		}
+	}
+
+	return 0;
+}
+#endif
+
+void exynos_rgt_dbg_snapshot_regulator(u32 val, unsigned long long time)
+{
+#ifdef CONFIG_DEBUG_SNAPSHOT_REGULATOR
+	u32 spd_ch, reg_addr, mod_addr, reg_data, reg_id;
+
+	spd_ch = (val >> 28) & 0xf;
+	reg_addr = (val >> 12) & 0xfff;
+	reg_data = (val >> 4) & 0xff;
+	mod_addr = (spd_ch << 12) | reg_addr;
+	reg_id = get_reg_id(spd_ch, reg_addr);
+
+	if (reg_id == REG_MAP_MAX)
+		dbg_snapshot_regulator(time, "CHKADDR", mod_addr, reg_data, reg_data, val & 0xF);
+	else
+		dbg_snapshot_regulator(time, rgt_ss_info.regulator_ss[spd_ch][reg_id].name, mod_addr,
+					get_reg_voltage(rgt_ss_info.regulator_ss[spd_ch][reg_id], reg_data),
+					reg_data, val & 0xf);
+#endif
+	return ;
+}
+
 static int exynos_rgt_probe(struct platform_device *pdev)
 {
 	int ret;
 	struct exynos_rgt_info *rgt_info;
-	struct device_node *regulators_np, *reg_np, *pmic_np;
-	int rgt_idx = 0, ipc_ch;
+	struct device_node *regulators_np, *reg_np;
+	int rgt_idx = 0;
 	const char *rgt_name;
-	struct regulator_ss_info *rgt_ss;
+#ifdef CONFIG_DEBUG_SNAPSHOT_REGULATOR
 	struct regulator_desc rdesc;
+	int rgt_ss_idx, ipc_ch, spd_ch;
+	struct device_node *pmic_np;
+	struct regulator_ss_info *rgt_ss;
+#endif
 
 	regulators_np = of_find_node_by_name(NULL, "regulators");
 	if (!regulators_np) {
@@ -293,8 +458,16 @@ static int exynos_rgt_probe(struct platform_device *pdev)
 		goto err_dbgfs_root;
 	}
 
+#ifdef CONFIG_DEBUG_SNAPSHOT_REGULATOR
+	ret = prepare_dss_regulator(pdev);
+	if (ret)
+		goto err_dbgfs_root;
+#endif
 	regulators_np = of_find_node_by_name(NULL, "regulators");
 	while (regulators_np) {
+#ifdef CONFIG_DEBUG_SNAPSHOT_REGULATOR
+		rgt_ss_idx = 0;
+#endif
 		for_each_child_of_node(regulators_np, reg_np) {
 			rgt_name = of_get_property(reg_np, "regulator-name", NULL);
 			if (!rgt_name)
@@ -320,26 +493,32 @@ static int exynos_rgt_probe(struct platform_device *pdev)
 			rgt_info[rgt_idx].f_volt =
 				debugfs_create_file("voltage", 0600, rgt_info[rgt_idx].reg_dir,
 						rgt_info[rgt_idx].rgt, &rgt_info[rgt_idx].volt_fops);
-
+#ifdef CONFIG_DEBUG_SNAPSHOT_REGULATOR
 			pmic_np = of_get_parent(regulators_np);
 			ret = of_property_read_u32(pmic_np, "acpm-ipc-channel", &ipc_ch);
 			if (!ret) {
-				rgt_ss = get_regulator_ss(rgt_idx);
-				if (rgt_ss != NULL) {
-					rdesc = *(rgt_info[rgt_idx].rgt->rdev->desc);
+				ret = of_property_read_u32(pmic_np, "acpm-speedy-channel", &spd_ch);
+				if (!ret) {
+					rgt_ss = get_regulator_ss(rgt_ss_idx++, spd_ch);
+					if (rgt_ss != NULL) {
+						rdesc = *(rgt_info[rgt_idx].rgt->rdev->desc);
 
-					snprintf(rgt_ss->name, sizeof(rgt_ss->name), "%s.", rdesc.name);
-					rgt_ss->min_uV = rdesc.min_uV;
-					rgt_ss->uV_step = rdesc.uV_step;
-					rgt_ss->linear_min_sel = rdesc.linear_min_sel;
-					rgt_ss->vsel_reg = rdesc.vsel_reg;
+						snprintf(rgt_ss->name, sizeof(rgt_ss->name), "%s.", rdesc.name);
+						rgt_ss->min_uV = rdesc.min_uV;
+						rgt_ss->uV_step = rdesc.uV_step;
+						rgt_ss->linear_min_sel = rdesc.linear_min_sel;
+						rgt_ss->vsel_reg = rdesc.vsel_reg;
+					}
 				}
 			}
+#endif
 			rgt_idx++;
 		}
 		regulators_np = of_find_node_by_name(regulators_np, "regulators");
 	}
-
+#ifdef CONFIG_DEBUG_SNAPSHOT_REGULATOR
+	set_reg_map();
+#endif
 	platform_set_drvdata(pdev, rgt_info);
 
 	return 0;
