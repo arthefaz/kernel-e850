65102baafb3f195999d8f043f13691fe9ac9d788
Kijun Kim <kijun.kim@samsung.com>
2019-07-05 14:56:51 +0900

    [3830] fbdev: dpu30: DPU driver is established
    Change-Id: If8b604148c2e0f55ccc1c206cf8b4ba92348db7b
    Signed-off-by: Kijun Kim <kijun.kim@samsung.com>

--- drivers/video/fbdev/exynos/dpu30/dpp.h
+++ drivers/video/fbdev/exynos/dpu30/dpp.h
@@ -32,6 +32,9 @@
 #if defined(CONFIG_SOC_EXYNOS9630)
 #include "./cal_9630/regs-dpp.h"
 #include "./cal_9630/dpp_cal.h"
+#elif defined(CONFIG_SOC_EXYNOS3830)
+#include "./cal_3830/regs-dpp.h"
+#include "./cal_3830/dpp_cal.h"
 #endif
 
 extern int dpp_log_level;
@@ -39,7 +42,11 @@ extern int dpp_log_level;
 #define DPP_MODULE_NAME		"exynos-dpp"
 
 #define MAX_DPP_CNT		7 /* + ODMA case */
-#if defined(CONFIG_SOC_EXYNOS9630)
+#if defined(CONFIG_SOC_EXYNOS9830)
+#define SOC_DPP_CNT		7 /* + ODMA case */
+#elif defined(CONFIG_SOC_EXYNOS9630)
+#define SOC_DPP_CNT		4 /* + ODMA case */
+#elif defined(CONFIG_SOC_EXYNOS3830)
 #define SOC_DPP_CNT		4 /* + ODMA case */
 #endif
 
--- drivers/video/fbdev/exynos/dpu30/dsim.h
+++ drivers/video/fbdev/exynos/dpu30/dsim.h
@@ -25,6 +25,9 @@
 #if defined(CONFIG_SOC_EXYNOS9630)
 #include "./cal_9630/regs-dsim.h"
 #include "./cal_9630/dsim_cal.h"
+#elif defined(CONFIG_SOC_EXYNOS3830)
+#include "./cal_3830/regs-dsim.h"
+#include "./cal_3830/dsim_cal.h"
 #endif
 
 extern int dsim_log_level;
