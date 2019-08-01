4d02aab22cdf5390813f972d978757b99f63217e
Jiun Yu <jiun.yu@samsung.com>
2019-08-01 09:56:19 +0900

    fbdev: dpu30: support to change vrefresh rate for S6E3HA9 panel
    Change-Id: Ie3f556ce9d1f9b02b8f3f99655ef91e1fc0b66db
    Signed-off-by: Jiun Yu <jiun.yu@samsung.com>

--- drivers/video/fbdev/exynos/dpu30/panels/panel-samsung-s6e3ha9.c
+++ drivers/video/fbdev/exynos/dpu30/panels/panel-samsung-s6e3ha9.c
@@ -83,6 +83,12 @@ static int s6e3ha9_displayon(struct exynos_panel_device *panel)
 #endif
 	dsim_write_data_seq(dsim, false, 0xC5, 0x0D, 0x10, 0xB4, 0x3E, 0x01);
 
+	/* vrefresh rate configuration */
+	if (panel->lcd_info.fps == 60)
+		dsim_write_data_seq(dsim, false, 0xBB, 0x05, 0x0C);
+	else if (panel->lcd_info.fps == 30)
+		dsim_write_data_seq(dsim, false, 0xBB, 0x05, 0x1C);
+
 	dsim_write_data_seq(dsim, false, 0x29); /* display on */
 
 	mutex_unlock(&panel->ops_lock);
@@ -148,6 +154,43 @@ static int s6e3ha9_set_light(struct exynos_panel_device *panel, u32 br_val)
 	return 0;
 }
 
+static int s6e3ha9_set_vrefresh(struct exynos_panel_device *panel, u32 refresh)
+{
+	struct dsim_device *dsim = get_dsim_drvdata(panel->id);
+
+	DPU_DEBUG_PANEL("%s +\n", __func__);
+	DPU_DEBUG_PANEL("applied vrefresh(%d), requested vrefresh(%d)\n",
+			panel->lcd_info.fps, refresh);
+
+	if (panel->lcd_info.fps == refresh) {
+		DPU_INFO_PANEL("prev and req fps are same(%d)\n", refresh);
+		return 0;
+	}
+
+	mutex_lock(&panel->ops_lock);
+
+	dsim_write_data_seq(dsim, false, 0xF0, 0x5A, 0x5A);
+
+	if (refresh == 60) {
+		dsim_write_data_seq(dsim, false, 0xBB, 0x05, 0x0C);
+	} else if (refresh == 30) {
+		dsim_write_data_seq(dsim, false, 0xBB, 0x05, 0x1C);
+	} else {
+		DPU_INFO_PANEL("not supported fps(%d)\n", refresh);
+		goto end;
+	}
+
+	panel->lcd_info.fps = refresh;
+
+end:
+	dsim_write_data_seq(dsim, false, 0xF0, 0xA5, 0xA5);
+
+	mutex_unlock(&panel->ops_lock);
+	DPU_DEBUG_PANEL("%s -\n", __func__);
+
+	return 0;
+}
+
 struct exynos_panel_ops panel_s6e3ha9_ops = {
 	.id		= {0x001090, 0x1310a1, 0x1330a1, 0x1300a1},
 	.suspend	= s6e3ha9_suspend,
@@ -158,4 +201,5 @@ struct exynos_panel_ops panel_s6e3ha9_ops = {
 	.dump		= s6e3ha9_dump,
 	.read_state	= s6e3ha9_read_state,
 	.set_light	= s6e3ha9_set_light,
+	.set_vrefresh	= s6e3ha9_set_vrefresh,
 };
