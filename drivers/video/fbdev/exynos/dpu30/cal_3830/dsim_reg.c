8af9411ee399facefa8b7a4741dec9f35c27f85b
Kijun Kim <kijun.kim@samsung.com>
2019-08-10 16:17:03 +0900

    [3830] fbdev: dpu30: cal: modified equation of cmd_te_ctrl to consider fps
    Change-Id: I1165f27ffdf2ac51d171650ffeee83c1acea6625
    Signed-off-by: Kijun Kim <kijun.kim@samsung.com>

--- drivers/video/fbdev/exynos/dpu30/cal_3830/dsim_reg.c
+++ drivers/video/fbdev/exynos/dpu30/cal_3830/dsim_reg.c
@@ -32,8 +32,7 @@
  */
 #define DSIM_CMD_ALLOW_VALUE		4
 #define DSIM_STABLE_VFP_VALUE		2
-#define TE_PROTECT_ON_TIME		158 /* 15.8ms*/
-#define TE_TIMEOUT_TIME			180 /* 18ms */
+#define TE_MARGIN					5 /* 5% */
 
 #define PLL_LOCK_CNT_MULT		500
 #define PLL_LOCK_CNT_MARGIN_RATIO	0	/* 10% ~ 20% */
@@ -1109,6 +1108,13 @@ static void dsim_reg_set_cmd_te_ctrl1(u32 id, u32 teprotecton, u32 tetout)
 	dsim_write(id, DSIM_CMD_TE_CTRL1, val);
 }
 
+static void dsim_reg_get_cmd_timer(unsigned int fps, unsigned int *te_protect,
+		unsigned int *te_timeout, u32 hs_clk)
+{
+	*te_protect = hs_clk * (100 - TE_MARGIN) * 100 / fps / 16;
+	*te_timeout = hs_clk * (100 + TE_MARGIN * 2) * 100 / fps / 16;
+}
+
 static void dsim_reg_set_cmd_ctrl(u32 id, struct exynos_panel_info *lcd_info,
 						struct dsim_clks *clks)
 {
@@ -1117,8 +1123,8 @@ static void dsim_reg_set_cmd_ctrl(u32 id, struct exynos_panel_info *lcd_info,
 	unsigned int time_te_tout;
 
 	time_stable_vfp = lcd_info->xres * DSIM_STABLE_VFP_VALUE * 3 / 100;
-	time_te_protect_on = (clks->hs_clk * TE_PROTECT_ON_TIME) / 16;
-	time_te_tout = (clks->hs_clk * TE_TIMEOUT_TIME) / 16;
+	dsim_reg_get_cmd_timer(lcd_info->fps, &time_te_protect_on,
+			&time_te_tout, clks->hs_clk);
 	dsim_reg_set_cmd_te_ctrl0(id, time_stable_vfp);
 	dsim_reg_set_cmd_te_ctrl1(id, time_te_protect_on, time_te_tout);
 }
