eeaefc02e545088ac41315dfa5ca29927e7bc905
Dongseong Kim <ds_in_ds.kim@samsung.com>
2019-10-23 15:54:03 +0900

    media: scaler: add flags of v4l2_ctrl about setFramerate
    v4l2_ctrl_handler doesn't pass ioctl(s_ctrl) to driver with same value.
    So, flag(V4L2_CTRL_FLAG_EXECUTE_ON_WRITE) is needed.
    
    Change-Id: Ic74b937b122254dfd18ba5aec6497573e9ee256f
    Signed-off-by: Dongseong Kim <ds_in_ds.kim@samsung.com>

--- drivers/media/platform/exynos/scaler/scaler-core.c
+++ drivers/media/platform/exynos/scaler/scaler-core.c
@@ -2471,6 +2471,7 @@ static const struct v4l2_ctrl_config sc_custom_ctrl[] = {
 		.id = SC_CID_FRAMERATE,
 		.name = "Frame rate setting",
 		.type = V4L2_CTRL_TYPE_INTEGER,
+		.flags = V4L2_CTRL_FLAG_EXECUTE_ON_WRITE,
 		.step = 1,
 		.min = 0,
 		.max = SC_FRAMERATE_MAX,
