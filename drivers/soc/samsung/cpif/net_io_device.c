fc7a7d9ba3f8579af2a0eafe1838e3334ae291aa
Dongseok Yi <dseok.yi@samsung.com>
2019-11-13 17:26:20 +0900

    [COMMON] soc: samsung: cpif: change net_device type to ARPHRD_RAWIP
    Android eBPF CLAT supports only ARPHRD_ETHER and ARPHRD_RAWIP
    
    Change-Id: I85c65add95eacdb63843eee032a4224fa7e95bed
    Signed-off-by: Dongseok Yi <dseok.yi@samsung.com>

--- drivers/soc/samsung/cpif/net_io_device.c
+++ drivers/soc/samsung/cpif/net_io_device.c
@@ -296,7 +296,7 @@ static const struct net_device_ops vnet_ops = {
 void vnet_setup(struct net_device *ndev)
 {
 	ndev->netdev_ops = &vnet_ops;
-	ndev->type = ARPHRD_PPP;
+	ndev->type = ARPHRD_RAWIP;
 	ndev->flags = IFF_POINTOPOINT | IFF_NOARP | IFF_MULTICAST;
 	ndev->addr_len = 0;
 	ndev->hard_header_len = 0;
