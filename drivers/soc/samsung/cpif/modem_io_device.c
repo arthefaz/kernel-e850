325e59dfa8d173e70ef5143a1077f2560e11d44b
Dongseok Yi <dseok.yi@samsung.com>
2019-11-18 12:49:55 +0900

    [COMMON] soc: samsung: cpif: use netif_receive_skb() instead of netif_rx()
    not use CONFIG_LINK_DEVICE_NAPI anymore
    
    Change-Id: Iad1676245660085a4fb8be428cb15c914563f068
    Signed-off-by: Dongseok Yi <dseok.yi@samsung.com>

--- drivers/soc/samsung/cpif/modem_io_device.c
+++ drivers/soc/samsung/cpif/modem_io_device.c
@@ -428,25 +428,15 @@ static int rx_multi_pdp(struct sk_buff *skb)
 #else
 	if (!check_gro_support(skb)) {
 #endif
-
-#ifdef CONFIG_LINK_DEVICE_NAPI
 		ret = netif_receive_skb(skb);
-#else /* !CONFIG_LINK_DEVICE_NAPI */
-		if (in_interrupt())
-			ret = netif_rx(skb);
-		else
-			ret = netif_rx_ni(skb);
-#endif /* CONFIG_LINK_DEVICE_NAPI */
-
 		if (ret != NET_RX_SUCCESS)
-			mif_err_limited("%s: %s<-%s: ERR! netif_rx\n",
+			mif_err_limited("%s: %s<-%s: ERR! netif_receive_skb\n",
 					ld->name, iod->name, iod->mc->name);
 	} else {
 		ret = napi_gro_receive(napi_get_current(), skb);
-		if (ret == GRO_DROP) {
+		if (ret == GRO_DROP)
 			mif_err_limited("%s: %s<-%s: ERR! napi_gro_receive\n",
 					ld->name, iod->name, iod->mc->name);
-		}
 
 		if (ld->gro_flush)
 			ld->gro_flush(ld);
