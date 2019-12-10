935d81ff0c01faf4be26be9ecb960512bd4d72d0
Jiyoung Jeong <ji_0.jeong@samsung.com>
2019-12-10 23:04:32 +0900

    [COMMON] soc: samsung: cpif: update version to CPIF-20191210R1
    Change-Id: I09301817f3d12065b89568d151570b25ab30b6f0
    Signed-off-by: Jiyoung Jeong <ji_0.jeong@samsung.com>

--- drivers/soc/samsung/cpif/cpif_version.h
+++ drivers/soc/samsung/cpif/cpif_version.h
@@ -15,6 +15,6 @@
 #ifndef __CPIF_VERSION_H__
 #define __CPIF_VERSION_H__
 
-const char cpif_driver_version[] = "CPIF-20191113R2";
+const char cpif_driver_version[] = "CPIF-20191210R1";
 
 #endif /* __CPIF_VERSION_H__ */
