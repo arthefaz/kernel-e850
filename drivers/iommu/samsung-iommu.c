kerneltoast/android_kernel_google_gs201

iommu/samsung: support for async fault mode
In case of SysMMU fault, driver uses stall mode which blocks fault
traffic and calls each device's fault handler to provide useful
information to find the cause of fault.

Async fault mode is supported for the devices which require error
response at fault time, and it would be able to recover from the fault
situation. It is optional because it depends on the characteristics of
device.

Change-Id: I3e07f80cbf884dd4dc6e385e36577156987ae616
Signed-off-by: Janghyuck Kim <janghyuck.kim@samsung.com>
(cherry picked from commit f7bb05a974429ab1d59f66bdc7f4b5114b5b9631)

drivers/iommu/samsung-iommu.c
@@ -198,9 +198,16 @@ static inline void __sysmmu_enable(struct sysmmu_drvdata *data)
 
 	writel(ctrl_val | CTRL_MMU_ENABLE, data->sfrbase + REG_MMU_CTRL);
 
-	if (data->has_vcr)
-		writel(CTRL_VID_ENABLE | CTRL_FAULT_STALL_MODE,
-		       data->sfrbase + REG_MMU_CTRL_VM);
+	if (data->has_vcr) {
+		ctrl_val = readl_relaxed(data->sfrbase + REG_MMU_CTRL_VM);
+
+		if (!data->async_fault_mode)
+			ctrl_val |= CTRL_FAULT_STALL_MODE;
+		else
+			ctrl_val &= ~CTRL_FAULT_STALL_MODE;
+
+		writel(ctrl_val | CTRL_VID_ENABLE, data->sfrbase + REG_MMU_CTRL_VM);
+	}
 }
 
 static struct samsung_sysmmu_domain *to_sysmmu_domain(struct iommu_domain *dom)
@@ -1050,6 +1057,10 @@ static int sysmmu_parse_dt(struct device *sysmmu, struct sysmmu_drvdata *data)
 			return ret;
 		}
 	}
+
+	/* use async fault mode */
+	data->async_fault_mode = of_property_read_bool(sysmmu->of_node,
+						       "sysmmu,async-fault");
 
 	ret = sysmmu_parse_tlb_property(sysmmu, data);
 	if (ret)

arthefaz/kernel-e850

drivers/iommu/samsung-iommu.c
@@ -189,9 +189,13 @@ static inline void __sysmmu_enable(struct sysmmu_drvdata *data)
 
 	writel(ctrl_val | CTRL_MMU_ENABLE, data->sfrbase + REG_MMU_CTRL);
 
-	if (data->has_vcr)
-		writel(CTRL_VID_ENABLE | CTRL_FAULT_STALL_MODE,
-		       data->sfrbase + REG_MMU_CTRL_VM);
+	if (data->has_vcr) {
+		ctrl_val = readl_relaxed(data->sfrbase + REG_MMU_CTRL_VM);
+
+		if (!data->async_fault_mode)
+			ctrl_val |= CTRL_FAULT_STALL_MODE;
+		writel(ctrl_val | CTRL_MMU_ENABLE, data->sfrbase + REG_MMU_CTRL_VM);
+	}
 }
 
 static struct samsung_sysmmu_domain *to_sysmmu_domain(struct iommu_domain *dom)
@@ -1051,6 +1055,10 @@ static int sysmmu_parse_dt(struct device *sysmmu, struct sysmmu_drvdata *data)
 			return ret;
 		}
 	}
+
+	/* use async fault mode */
+	data->async_fault_mode = of_property_read_bool(sysmmu->of_node,
+						       "sysmmu,async-fault");
 
 	ret = sysmmu_parse_tlb_property(sysmmu, data);
 	if (ret)