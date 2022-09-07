5500af5497b580f68d3c49df72c34640af50afaf
Khalid Shaik <khalid.s@samsung.com>
2022-09-08 01:12:59 +0530

    Revert "enabled hs400es mode and resolved compilation errors"
    This reverts commit ea53deda046a14056d0c1814eb423e93af8b72c4.
    
    Change-Id: I69759c821b08ff11e1da8c075a8975314bdf6c40

--- drivers/mmc/host/dw_mmc.h
+++ drivers/mmc/host/dw_mmc.h
@@ -16,9 +16,6 @@
 #include <linux/reset.h>
 #include <linux/interrupt.h>
 
-/* Enables ultra low power mode */
-#define DW_MCI_QUIRK_ENABLE_ULP                 BIT(6)
-
 enum dw_mci_state {
 	STATE_IDLE = 0,
 	STATE_SENDING_CMD,
@@ -252,7 +249,6 @@ struct dma_pdata;
 struct dw_mci_board {
 	unsigned int bus_hz; /* Clock speed at the cclk_in pad */
 
-	u32 quirks;             /* Workaround / Quirk flags */
 	u32 caps;	/* Capabilities */
 	u32 caps2;	/* More capabilities */
 	u32 pm_caps;	/* PM capabilities */
