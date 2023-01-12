1d432695d9e0e3d0047c8f02fc3f50b8a85ae6be
Oh Eomji <eomji.oh@samsung.com>
2023-01-12 11:26:59 +0900

    Revert "[COMMON] usb: gadget: modify gadget->connected location"
    This reverts commit 56f0578d432127f66f21b3b6da79ac60723f6563.
    
    Change-Id: Ia9d894110ac1cd4332bc0068a214c0139f6a20e4
    Signed-off-by: Oh Eomji <eomji.oh@samsung.com>

--- drivers/usb/gadget/udc/core.c
+++ drivers/usb/gadget/udc/core.c
@@ -684,10 +684,9 @@ int usb_gadget_connect(struct usb_gadget *gadget)
 		goto out;
 	}
 
-	gadget->connected = 1;
 	ret = gadget->ops->pullup(gadget, 1);
-	if (ret)
-		gadget->connected = 0;
+	if (!ret)
+		gadget->connected = 1;
 
 out:
 	trace_usb_gadget_connect(gadget, ret);
