7c236912ea9ba27f9fb611c441d24d68baa4edb1
Sam Protsenko <semen.protsenko@linaro.org>
2023-03-07 20:35:41 -0600

    dt-bindings: power: pd-samsung: Add Exynos850 support
    Document the compatible string for Exynos850 power domains controller.
    Also add power domain indices which can be used in "samsung,pd-index"
    property to specify a particular power domain in the device tree.
    
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>

---
+++ include/dt-bindings/power/samsung,exynos850-power.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (c) 2023 Linaro Ltd.
+ * Author: Sam Protsenko <semen.protsenko@linaro.org>
+ */
+
+#ifndef _DT_BINDINGS_POWER_EXYNOS850_POWER_H
+#define _DT_BINDINGS_POWER_EXYNOS850_POWER_H
+
+#define EXYNOS850_PD_HSI		0
+#define EXYNOS850_PD_G3D		1
+#define EXYNOS850_PD_MFCMSCL		2
+#define EXYNOS850_PD_DPU		3
+#define EXYNOS850_PD_AUD		4
+#define EXYNOS850_PD_IS			5
+
+#endif /* _DT_BINDINGS_POWER_EXYNOS850_POWER_H */
