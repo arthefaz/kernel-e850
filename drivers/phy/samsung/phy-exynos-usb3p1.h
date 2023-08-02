7ba2c5ae7a8d2478f09378edfabecca7ddb71f16
Sam Protsenko <semen.protsenko@linaro.org>
2023-08-01 23:19:46 -0500

    phy: samsung: exynos-usbdrd: Remove dead code in usb3p1
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>

--- drivers/phy/samsung/phy-exynos-usb3p1.h
+++ drivers/phy/samsung/phy-exynos-usb3p1.h
@@ -24,29 +24,11 @@
 extern void phy_exynos_usb_v3p1_enable(struct exynos_usbphy_info *info);
 extern void phy_exynos_usb_v3p1_disable(struct exynos_usbphy_info *info);
 extern void phy_exynos_usb_v3p1_link_sw_reset(struct exynos_usbphy_info *info);
-extern void phy_exynos_usb3p1_pcs_reset(struct exynos_usbphy_info *info);
-extern void phy_exynos_usb3p1_sw_rst(struct exynos_usbphy_info *info);
-extern void phy_exynos_usb_v3p1_enable_dp_pullup(struct exynos_usbphy_info *info);
-extern void phy_exynos_usb_v3p1_disable_dp_pullup(struct exynos_usbphy_info *info);
+
 /* USB/DP PHY control */
-extern void phy_exynos_usb_v3p1_pma_ready(struct exynos_usbphy_info *info);
 extern void phy_exynos_usb_v3p1_g2_pma_ready(struct exynos_usbphy_info *info);
-extern void phy_exynos_usb_v3p1_g2_disable(struct exynos_usbphy_info *info);
-extern void phy_exynos_usb_v3p1_pma_sw_rst_release(struct exynos_usbphy_info *info);
-extern void phy_exynos_usb_v3p1_g2_link_pclk_sel(struct exynos_usbphy_info *info);
-extern void phy_exynos_usb_v3p1_g2_pma_sw_rst_release(struct exynos_usbphy_info *info);
 extern void phy_exynos_usb_v3p1_pipe_ovrd(struct exynos_usbphy_info *info);
-extern void phy_exynos_usb_v3p1_pipe_ready(struct exynos_usbphy_info *info);
 
-extern void phy_exynos_usb_v3p1_config_host_mode(struct exynos_usbphy_info *info);
-
-/* 2.0 PHY Test I/F Access */
-extern u8 phy_exynos_usb_v3p1_tif_ov_rd(struct exynos_usbphy_info *info, u8 addr);
-extern u8 phy_exynos_usb_v3p1_tif_ov_wr(struct exynos_usbphy_info *info, u8 addr, u8 data);
-extern u8 phy_exynos_usb_v3p1_tif_sts_rd(struct exynos_usbphy_info *info, u8 addr);
-/* FS_VPLUS/VMINUS interrupt */
-extern void phy_exynos_usb3p1_set_fs_vplus_vminus(struct exynos_usbphy_info *usbphy_info, u32 fsls_speed_sel, u32 fsv_out_en);
-extern u8 phy_exynos_usb3p1_get_fs_vplus_vminus(struct exynos_usbphy_info *info);
 /* UART/JTAG over USB */
 void phy_exynos_usb3p1_set_fsv_out_en(struct exynos_usbphy_info *usbphy_info, u32 fsv_out_en);
 
